d0e3e03224bb937e22fa848a64857bb2c259a8b7
Taifur2Tanaim <taifurmohammadt@gmail.com>
2023-12-26 12:50:17 +0600

    Day 01, viewBag, viewData, action, view

---
+++ practice01/Controllers/PersonController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace practice01.Controllers
+{
+    public class PersonController : Controller
+    {
+        public ActionResult List()
+        {
+            int a = 10;
+            int b = 20;
+            int c = a + b;
+
+            ViewBag.a = a;
+            ViewBag.b = b;
+            ViewBag.c = c;
+
+            string[] names = new string[3];
+            names[0] = "Tanvir";
+            names[1] = "Sabbir";
+            names[2] = "Hasan";
+
+            ViewBag.Names = names;
+            return View();
+        }
+        public ActionResult CreatePerson()
+        {
+            return View();
+        }
+    }
+}
