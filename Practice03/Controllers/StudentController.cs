9c2156dfd4d7e2c65776c4b13a66906575c633cb
Taifur2Tanaim <taifurmohammadt@gmail.com>
2023-12-28 23:22:13 +0600

    practice of n-tier architecture

---
+++ Practice03/Controllers/StudentController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Practice03.Models;
+namespace Practice03.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        public ActionResult CreateSubmit(Student s) { 
+            // 4 ways of receving value from form to action 
+            // From httpRequestBase :- Request(instance) ; 
+            return View(s);
+        }
+    }
+}
---
+++ Practice03/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Practice03.Models
+{
+    public class Student
+    {
+        public string Name {  get; set; }
+        public string Id { get; set; }
+        public string Dob { get; set; }
+
+    }
+}
